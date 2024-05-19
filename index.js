2fe117154fe55548de7d0e3f9994a6abf866d762
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-05-19 19:31:54 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function foo()
+{
+    var firstname=document.getElementById("firstname").value;
+    var middlename=document.getElementById("middlename").value;
+    var lastname=document.getElementById("lastname").value;
+    var email=document.getElementById("email").value;
+    var phonenumber=document.getElementById("phonenumber").value;
+
+console.log(firstname);
+console.log( middlename);
+console.log( lastname);
+console.log( email);
+console.log( phonenumber);
+}
+
+
+
